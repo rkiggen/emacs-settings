3a16adde2a4e6d4f4d1fb6129a57e9fcd9a8fb83
Robby Kiggen <robby.kiggen@bdo.global>
2017-04-19 10:45:39 +0200

    Initial version

---
+++ init.el
@@ -0,0 +1,26 @@
+;; Rob's .emacs file
+;;
+;; Author: Rob Kiggen <robby.kiggen@essential-it.be>
+
+;; Load the config
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(require 'org)
+(org-babel-load-file (concat user-emacs-directory "config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (use-package org-bullets evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
